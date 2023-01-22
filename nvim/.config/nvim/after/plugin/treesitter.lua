8f9a23bf917ca8aa004fb2b9b8bf865efab3885a
Taylor Thompson <taylor@nightshiftwaxco.com>
2023-01-21 20:54:30 -0500

    added tsx to treesitter

--- nvim/.config/nvim/after/plugin/treesitter.lua
+++ nvim/.config/nvim/after/plugin/treesitter.lua
@@ -1,18 +1,18 @@
 require('nvim-treesitter.configs').setup {
-  ensure_installed = { "help", "javascript", "typescript", "c", "lua", "rust" },
+  ensure_installed = { "help", "javascript", "typescript", "tsx", "c", "lua", "rust" },
   highlight = {
     enable = true,
   },
-    custom_captures = {
-      -- Highlight the @foo.bar capture group with the "Identifier" highlight group.
-      ["foo.bar"] = "Identifier",
-    },
-    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-    -- Using this option may slow down your editor, and you may see some duplicate highlights.
-    -- Instead of true it can also be a list of languages
-    additional_vim_regex_highlighting = false,
-    context_commentstring = {
+  custom_captures = {
+    -- Highlight the @foo.bar capture group with the "Identifier" highlight group.
+    ["foo.bar"] = "Identifier",
+  },
+  -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+  -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+  -- Using this option may slow down your editor, and you may see some duplicate highlights.
+  -- Instead of true it can also be a list of languages
+  additional_vim_regex_highlighting = false,
+  context_commentstring = {
     enable = true,
     enable_autocmd = false,
 
@@ -26,8 +26,8 @@ require('nvim-treesitter.configs').setup {
         comment = '// %s',
         __parent = {
           jsx_expression = '// %s',
-          },
         },
+      },
       javascript = {
         __default = '// %s',
         __multiline = '// %s',
@@ -37,8 +37,9 @@ require('nvim-treesitter.configs').setup {
         comment = '// %s',
         __parent = {
           jsx_expression = '// %s',
-          },
-        }
+        },
       }
+    }
 
-}}
+  }
+}
