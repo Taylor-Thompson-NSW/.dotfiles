b9a4dc465defd81ad0f88f72f87d8d310c94ad7b
Taylor Thompson <taylor@nightshiftwaxco.com>
2023-02-26 03:47:08 -0500

    added lock and suspend shortcut mainly. check diffs for others

--- nvim/.config/nvim/lua/tayl0rgen/packer.lua
+++ nvim/.config/nvim/lua/tayl0rgen/packer.lua
@@ -5,7 +5,7 @@ return require("packer").startup(function(use)
   use("mhinz/vim-startify")
 
   --Snippets
-  use({"L3MON4D3/LuaSnip", tag = "v<CurrentMajor>.*"})
+  use({ "L3MON4D3/LuaSnip", tag = "v<CurrentMajor>.*" })
   use("rafamadriz/friendly-snippets")
 
   --Code Comment plugins
@@ -15,28 +15,27 @@ return require("packer").startup(function(use)
   -- Close tags
   use("alvan/vim-closetag")
 
-
   --LSP
   use {
-	  'VonHeikemen/lsp-zero.nvim',
-	  requires = {
-		  -- LSP Support
-		  {'neovim/nvim-lspconfig'},
-		  {'williamboman/mason.nvim'},
-		  {'williamboman/mason-lspconfig.nvim'},
+    'VonHeikemen/lsp-zero.nvim',
+    requires = {
+      -- LSP Support
+      { 'neovim/nvim-lspconfig' },
+      { 'williamboman/mason.nvim' },
+      { 'williamboman/mason-lspconfig.nvim' },
 
-		  -- Autocompletion
-		  {'hrsh7th/nvim-cmp'},
-		  {'hrsh7th/cmp-buffer'},
-		  {'hrsh7th/cmp-path'},
-		  {'saadparwaiz1/cmp_luasnip'},
-		  {'hrsh7th/cmp-nvim-lsp'},
-		  {'hrsh7th/cmp-nvim-lua'},
+      -- Autocompletion
+      { 'hrsh7th/nvim-cmp' },
+      { 'hrsh7th/cmp-buffer' },
+      { 'hrsh7th/cmp-path' },
+      { 'saadparwaiz1/cmp_luasnip' },
+      { 'hrsh7th/cmp-nvim-lsp' },
+      { 'hrsh7th/cmp-nvim-lua' },
 
-		  -- Snippets
-		  {'L3MON4D3/LuaSnip'},
-		  {'rafamadriz/friendly-snippets'},
-	  }
+      -- Snippets
+      { 'L3MON4D3/LuaSnip' },
+      { 'rafamadriz/friendly-snippets' },
+    }
   }
   --
 
@@ -57,7 +56,7 @@ return require("packer").startup(function(use)
 
 
   --Treesitter -----------------
-  use {"nvim-treesitter/nvim-treesitter",  run = ":TSUpdate"}
+  use { "nvim-treesitter/nvim-treesitter", run = ":TSUpdate" }
   use("nvim-treesitter/nvim-treesitter-context")
   --use("p00f/nvim-ts-rainbow")
   ------------------------------
@@ -76,11 +75,10 @@ return require("packer").startup(function(use)
 
   --COLOR SCHEMES
   use("sainnhe/gruvbox-material")
-  use{"folke/tokyonight.nvim", branch = "main" }
+  use { "folke/tokyonight.nvim", branch = "main" }
 
   -----------------------------
   --Jupyter & Neovim plugin - Search specifically for: Magma
-  use{"dccsillag/magma-nvim",  run =  ":UpdateRemotePlugins" }
-  end
+  use { "dccsillag/magma-nvim", run = ":UpdateRemotePlugins" }
+end
 )
-
